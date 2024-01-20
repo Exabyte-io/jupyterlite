d5e3ef42bbf39c858b215939e0af148c683c88fa
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-01-19 17:09:08 -0800

    chore: point to github

--- extensions/jupyterlab-iframe-bridge-example/src/__tests__/jupyterlab-iframe-bridge-example.spec.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Example of [Jest](https://jestjs.io/docs/getting-started) unit tests
- */
-
-describe('jupyterlab-iframe-bridge-example', () => {
-  it('should be tested', () => {
-    expect(1 + 1).toEqual(2);
-  });
-});
