885e428bb2ef08d57ecd89f670348101189bf0a1
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-01-23 21:06:16 -0800

    update: use app to pass data to kernel

--- extensions/materials_designer_bridge/src/index.ts
+++ extensions/materials_designer_bridge/src/index.ts
@@ -11,12 +11,47 @@ const plugin: JupyterFrontEndPlugin<void> = {
   description: 'Extension to pass materials data between Materials Designer and Jupyter Lite instance',
   autoStart: true,
   activate: (app: JupyterFrontEnd) => {
-    console.log('JupyterLab extension materials-designer-bridge is activated!');
-  
+    console.log('MD Extension. JupyterLab extension materials-designer-bridge is activated!');
+
+    // Define a command that sends data to the kernel
+    const commandId = 'my-extension:send-data-to-kernel';
+
+    app.commands.addCommand(commandId, {
+      execute: () => {
+        // Get the current kernel 
+        const kernel = app.serviceManager.sessions.findByPath('debugging_jl.ipynb');
+        console.log('MD Extension. Kernel:', kernel);
+        // Send a message to the kernel
+        kernel.then(session => {
+          // @ts-ignore
+          session.kernel.requestExecute({ code: `get_materials(${JSON.stringify(window.materials)})` });
+        });
+      }
+    });
+
     /* Incoming messages management */
     window.addEventListener('message', event => {
+      console.log('MD Extension. Event received from the host:', event);
       if (event.data.type === 'from-host-to-iframe') {
-        console.log('Message received in the iframe:', event.data);
+        console.log('MD Extension. Message received in the iframe:', event.data);
+        let materials = event.data.materials;
+        console.log('MD Extension. Materials received in the iframe:', materials);
+        // @ts-ignore
+        window.materials = materials;
+        // @ts-ignore
+        console.log('MD Extension. Materials stored in the iframe:', window.materials);
+
+
+        // TODO: reserve in case previous approach doesn't work
+        // const kernel = app.serviceManager.sessions.findByPath('path-to-your-notebook');
+        // kernel.then(session => {
+        //   let materials = event.data.materials;
+        //   session.kernel.requestExecute({ code: `your_python_function(${JSON.stringify(materials)})` });
+        // });
+
+
+        // Execute the command to send data to the kernel
+        app.commands.execute(commandId);
       }
     });
 
@@ -25,10 +60,10 @@ const plugin: JupyterFrontEndPlugin<void> = {
     const sendMaterialsData = (): void => {
       const message = {
         type: 'from-iframe-to-host',
-        materials: "supposed to be materials data"
+        materials: "MD Extension. supposed to be materials data"
       };
       window.parent.postMessage(message, '*');
-      console.log('Message sent to the host:', message);
+      console.log('MD Extension. Message sent to the host:', message);
     }
   }
 };
