c2585658c7323050c3255ad18c87e0e990edebd5
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-01-27 19:58:26 -0800

    update: add to pymatgen function

--- content/definitions.py
+++ content/definitions.py
@@ -28,3 +28,32 @@ def get_materials():
 
     display(Javascript(js_code))
     print("Status: materials updated")
+
+
+from pymatgen.core import Structure, Lattice
+
+
+def to_pymatgen(material_data):
+    lattice_params = material_data["lattice"]
+    lattice_vectors = lattice_params["vectors"]
+    a = lattice_vectors["a"]
+    b = lattice_vectors["b"]
+    c = lattice_vectors["c"]
+
+    # Create a Lattice
+    lattice = Lattice([a, b, c])
+
+    # Extract the basis information
+    basis = material_data["basis"]
+    elements = [element["value"] for element in basis["elements"]]
+    coordinates = [coord["value"] for coord in basis["coordinates"]]
+
+    # Assuming that the basis units are fractional since it's a crystal basis
+    coords_are_cartesian = basis["units"].lower() != "crystal"
+
+    # Create the Structure
+    structure = Structure(
+        lattice, elements, coordinates, coords_are_cartesian=coords_are_cartesian
+    )
+
+    return structure
