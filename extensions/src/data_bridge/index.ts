9a28c4fa0ae896f1a248b4c3fb02d3c72a17d8a9
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-03-16 18:03:51 -0700

    update: listen to kernel status change

--- extensions/src/data_bridge/index.ts
+++ extensions/src/data_bridge/index.ts
@@ -1,10 +1,11 @@
+// @ts-nocheck
 import {
     JupyterFrontEnd,
     JupyterFrontEndPlugin,
 } from "@jupyterlab/application";
 
-import {NotebookPanel, INotebookTracker} from "@jupyterlab/notebook";
-import {JupyterliteMessageSchema} from "@mat3ra/esse/lib/js/types";
+import {NotebookPanel, INotebookTracker, NotebookAdapter} from "@jupyterlab/notebook";
+import {IframeMessageSchema} from "@mat3ra/esse/lib/js/types";
 
 /**
  * Initialization data for the data-bridge extension.
@@ -18,27 +19,63 @@ const plugin: JupyterFrontEndPlugin<void> = {
     requires: [INotebookTracker],
     activate: async (
         app: JupyterFrontEnd,
-        notebookTracker: INotebookTracker
+        notebookTracker: INotebookTracker,
+        notebookAdapter: NotebookAdapter
     ) => {
         console.log("JupyterLab extension data-bridge is activated!");
 
-        // Send path of the currently opened notebook to the host page when the notebook is opened
-        notebookTracker.currentChanged.connect((sender, notebookPanel) => {
-            if (notebookPanel) {
-                const currentPath = notebookPanel.context.path;
+        // variable to hold the data from the host page
+        // @ts-ignore
+        app.dataFromHost = "";
 
-                window.parent.postMessage(
-                    {
-                        type: "from-iframe-to-host",
-                        action: "set-data",
-                        payload: {
-                            data: currentPath,
-                        },
-                    },
-                    "*"
-                );
+        // @ts-ignore
+        notebookTracker.currentChanged.connect(async (sender, notebookPanel: NotebookPanel) => {
+            if (notebookPanel) {
+                console.log("Notebook opened", notebookPanel.context.path);
+                await notebookPanel.sessionContext.ready;
+                const sessionContext = notebookPanel.sessionContext;
+
+                sessionContext.kernelChanged.connect((_, kernel) => {
+                    console.log("Kernel changed", kernel);
+                    console.log("sessionContext.kernel", sessionContext);
+                });
+
+                sessionContext.session?.kernel?.statusChanged.connect((_, status) => {
+                    console.log("Kernel status changed", status);
+                    console.log("_", _);
+                });
+
+                if (notebookPanel.sessionContext.session?.kernel?.status === 'idle') {
+                    console.log("Kernel is idle");
+                    const kernel = notebookPanel.sessionContext.session.kernel;
+                    // @ts-ignore
+                    console.log("dataFromHost", app.dataFromHost);
+                    // @ts-ignore
+                    kernel.requestExecute({ code: `data = ${app.dataFromHost}` });
+                }
             }
         });
+
+
+        // Similar to https://jupyterlab.readthedocs.io/en/stable/api/classes/application.LabShell.html#currentWidget
+        // https://jupyterlite.readthedocs.io/en/latest/reference/api/ts/interfaces/jupyterlite_application.ISingleWidgetShell.html#currentwidget
+        const currentWidget = app.shell.currentWidget;
+        if (currentWidget instanceof NotebookPanel) {
+            const notebookPanel = currentWidget;
+            const sessionContext = notebookPanel.sessionContext;
+            sessionContext.kernelChanged.connect((_, kernel) => {
+                console.log("Kernel changed", kernel);
+            });
+            const kernel = notebookPanel.sessionContext.session?.kernel;
+            if (kernel) {
+                // @ts-ignore
+                kernel.requestExecute(`data = ${app.dataFromHost}`);
+            } else {
+                console.error("No active kernel found");
+            }
+        } else {
+            console.error("Current active widget is not a notebook");
+        }
 
         // @ts-ignore
         window.sendDataToHost = (data: any) => {
@@ -54,48 +91,21 @@ const plugin: JupyterFrontEndPlugin<void> = {
             );
         };
 
-        // @ts-ignore
-        window.requestDataFromHost = (variableName = "data") => {
-            console.log("requestDataFromHost", variableName);
-            window.parent.postMessage(
-                {
-                    type: "from-iframe-to-host",
-                    action: "get-data",
-                    payload: {
-                        variableName,
-                    },
-                },
-                "*"
-            );
-        };
 
-        window.addEventListener("message", async (event: MessageEvent<JupyterliteMessageSchema>) => {
+        // @ts-ignore
+        window.addEventListener("message", async (event: MessageEvent<IframeMessageSchema>) => {
             if (event.data.type === "from-host-to-iframe") {
                 let data = event.data.payload.data;
-                let variableName = event.data.payload.variableName || "data";
                 const dataJson = JSON.stringify(data);
-                const code = `
-  import json
-  ${variableName} = json.loads('${dataJson}')
-      `;
-                // Similar to https://jupyterlab.readthedocs.io/en/stable/api/classes/application.LabShell.html#currentWidget
-                // https://jupyterlite.readthedocs.io/en/latest/reference/api/ts/interfaces/jupyterlite_application.ISingleWidgetShell.html#currentwidget
-                const currentWidget = app.shell.currentWidget;
-
-                if (currentWidget instanceof NotebookPanel) {
-                    const notebookPanel = currentWidget;
-                    const kernel = notebookPanel.sessionContext.session?.kernel;
-                    if (kernel) {
-                        kernel.requestExecute({code: code});
-                    } else {
-                        console.error("No active kernel found");
-                    }
-                } else {
-                    console.error("Current active widget is not a notebook");
-                }
+                // @ts-ignore
+                app.dataFromHost = dataJson;
+                //@ts-ignore
+                console.log("Data from host received. app:", app.dataFromHost);
             }
+
         });
     },
 };
+
 
 export default plugin;
