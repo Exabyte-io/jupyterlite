2e399ae1953b2968856cd848db1048e1dc72a9ed
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-03-19 15:36:02 -0700

    chore: add types gymnastics

--- extensions/src/data_bridge/index.ts
+++ extensions/src/data_bridge/index.ts
@@ -1,71 +1,75 @@
-// @ts-nocheck
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import {
     JupyterFrontEnd,
-    JupyterFrontEndPlugin,
-} from "@jupyterlab/application";
+    JupyterFrontEndPlugin
+} from '@jupyterlab/application';
 
-import {NotebookPanel, INotebookTracker, NotebookAdapter} from "@jupyterlab/notebook";
-import {IframeMessageSchema} from "@mat3ra/esse/lib/js/types";
+import { IKernelConnection } from '@jupyterlab/services/lib/kernel/kernel';
+import { NotebookPanel, INotebookTracker } from '@jupyterlab/notebook';
+import { IframeMessageSchema } from '@mat3ra/esse/lib/js/types';
+
+interface IExtendedJupyterFrontEnd extends JupyterFrontEnd {
+    dataFromHost: string;
+}
 
 /**
  * Initialization data for the data-bridge extension.
  * Similar to https://jupyterlite.readthedocs.io/en/latest/howto/configure/advanced/iframe.html
  */
 const plugin: JupyterFrontEndPlugin<void> = {
-    id: "data-bridge:plugin",
+    id: 'data-bridge:plugin',
     description:
-        "Extension to pass JSON data between host page and Jupyter Lite instance",
+        'Extension to pass JSON data between host page and Jupyter Lite instance',
     autoStart: true,
     requires: [INotebookTracker],
-    activate: async (
-        app: JupyterFrontEnd,
-        notebookTracker: INotebookTracker,
-        notebookAdapter: NotebookAdapter
-    ) => {
-        console.log("JupyterLab extension data-bridge is activated!");
-
+    activate: async (app: JupyterFrontEnd, notebookTracker: INotebookTracker) => {
+        console.log('JupyterLab extension data-bridge is activated!');
+        const extendedApp = app as IExtendedJupyterFrontEnd;
         // Reusing the `app` variable to hold the data from the host page, accessible from any notebook and kernel
-        // @ts-ignore
-        app.dataFromHost = "";
+        extendedApp.dataFromHost = '';
 
         const MESSAGE_GET_DATA_CONTENT = {
-            type: "from-iframe-to-host",
-            action: "get-data",
+            type: 'from-iframe-to-host',
+            action: 'get-data',
             payload: {}
         };
 
         // On JupyterLite startup send get-data message to the host to request data
-        window.parent.postMessage(
-            MESSAGE_GET_DATA_CONTENT,
-            "*"
-        );
-
+        window.parent.postMessage(MESSAGE_GET_DATA_CONTENT, '*');
 
         /**
          * Listen for the current notebook being changed, and on kernel status change load the data into the kernel
          */
-        // @ts-ignore
-        notebookTracker.currentChanged.connect(async (sender, notebookPanel: NotebookPanel) => {
-            if (notebookPanel) {
-                console.debug("Notebook opened", notebookPanel.context.path);
-                await notebookPanel.sessionContext.ready;
-                const sessionContext = notebookPanel.sessionContext;
+        notebookTracker.currentChanged.connect(
+            // @ts-ignore
+            async (sender, notebookPanel: NotebookPanel) => {
+                if (notebookPanel) {
+                    console.debug('Notebook opened', notebookPanel.context.path);
+                    await notebookPanel.sessionContext.ready;
+                    const sessionContext = notebookPanel.sessionContext;
 
-                sessionContext.session?.kernel?.statusChanged.connect((kernel, status) => {
-                    // @ts-ignore
-                    if (status === 'idle' && kernel.dataFromHost !== app.dataFromHost) {
-                        // Save previous data inside the current kernel to avoid reloading the same data
-                        // @ts-ignore
-                        kernel.dataFromHost = app.dataFromHost;
-                        loadData(kernel, app.dataFromHost);
-                    }
-                    // Reset the flag when the kernel is restarting, since this flag is not affected by the kernel restart
-                    if (status === 'restarting') {
-                        kernel.dataFromHost = "";
-                    }
-                });
+                    sessionContext.session?.kernel?.statusChanged.connect(
+                        (kernel, status) => {
+                            if (
+                                status === 'idle' &&
+                                // @ts-ignore
+                                kernel.dataFromHost !== extendedApp.dataFromHost
+                            ) {
+                                // Save previous data inside the current kernel to avoid reloading the same data
+                                // @ts-ignore
+                                kernel.dataFromHost = extendedApp.dataFromHost;
+                                loadData(kernel, extendedApp.dataFromHost);
+                            }
+                            // Reset the flag when the kernel is restarting, since this flag is not affected by the kernel restart
+                            if (status === 'restarting') {
+                                // @ts-ignore
+                                kernel.dataFromHost = '';
+                            }
+                        }
+                    );
+                }
             }
-        });
+        );
 
         /**
          * Send data to the host page
@@ -74,14 +78,11 @@ const plugin: JupyterFrontEndPlugin<void> = {
         // @ts-ignore
         window.sendDataToHost = (data: object) => {
             const MESSAGE_SET_DATA_CONTENT = {
-                type: "from-iframe-to-host",
-                action: "set-data",
+                type: 'from-iframe-to-host',
+                action: 'set-data',
                 payload: data
             };
-            window.parent.postMessage(
-                MESSAGE_SET_DATA_CONTENT,
-                "*"
-            );
+            window.parent.postMessage(MESSAGE_SET_DATA_CONTENT, '*');
         };
 
         /**
@@ -89,36 +90,34 @@ const plugin: JupyterFrontEndPlugin<void> = {
          * @param event MessageEvent
          */
         // @ts-ignore
-        window.addEventListener("message", async (event: MessageEvent<IframeMessageSchema>) => {
-            if (event.data.type === "from-host-to-iframe") {
-                // @ts-ignore
-                app.dataFromHost = JSON.stringify(event.data.payload);
-                // Execute code in the kernel
-                const notebookPanel = notebookTracker.currentWidget;
-                await notebookPanel.sessionContext.ready;
-                const sessionContext = notebookPanel.sessionContext;
-                const kernel = sessionContext.session?.kernel;
-                // @ts-ignore
-                loadData(kernel, app.dataFromHost);
+        window.addEventListener(
+            'message',
+            async (event: MessageEvent<IframeMessageSchema>) => {
+                if (event.data.type === 'from-host-to-iframe') {
+                    extendedApp.dataFromHost = JSON.stringify(event.data.payload);
+                    const notebookPanel = notebookTracker.currentWidget;
+                    await notebookPanel?.sessionContext.ready;
+                    const sessionContext = notebookPanel?.sessionContext;
+                    const kernel = sessionContext?.session?.kernel;
+                    if (kernel) {
+                        loadData(kernel, extendedApp.dataFromHost);
+                    }
+                }
             }
-        });
+        );
 
         /**
          * Load the data into the kernel by executing code
          * @param kernel
-         * @param data
+         * @param data string representation of JSON
          */
-        const loadData = (kernel: IKernelConnection, data: JSON) => {
+        const loadData = (kernel: IKernelConnection, data: string) => {
             const dataFromHostString = JSON.stringify(data);
             const code = `import json\ndata_from_host = json.loads(${dataFromHostString})`;
-            // @ts-ignore
-            const result = kernel.requestExecute({code: code});
-            // @ts-ignore
-            console.debug("Execution result", result);
-        }
-
-    },
+            const result = kernel.requestExecute({ code: code });
+            console.debug('Execution result', result);
+        };
+    }
 };
-
 
 export default plugin;
