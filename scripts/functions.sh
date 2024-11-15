5a20a0317138d78964879e45175bc687b19f7d97
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-11-15 14:18:32 -0800

    try: realpath

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -49,11 +49,9 @@ build_extension() {
     # Our extension is treated as a separate package in ./extensions/src directory
     # Copy the extension source file that we changed to the cookiecutter template in ./extensions/dist directory
     # It places the source file in the dist/EXTENSION_NAME/src file that will be used for the build
-    SRC_FILE="${PACKAGE_ROOT_PATH}/extensions/src/${EXTENSION_NAME}/index.ts"
-    DEST_FILE="${PACKAGE_ROOT_PATH}/extensions/dist/${EXTENSION_NAME}/src/index.ts"
+    SRC_FILE="$(realpath "${PACKAGE_ROOT_PATH}/extensions/src/${EXTENSION_NAME}/index.ts)")"
+    DEST_FILE="$(realpath "${PACKAGE_ROOT_PATH}/extensions/dist/${EXTENSION_NAME}/src/index.ts")"
     echo "DEBUG: copying $SRC_FILE to $DEST_FILE"
-    echo SRC_FILE
-    echo DEST_FILE
 
     if [ -f "$SRC_FILE" ] && [ -d "$DEST_FILE" ]; then
         cp "$SRC_FILE" "$DEST_FILE"
