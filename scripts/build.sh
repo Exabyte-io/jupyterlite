2f60d88b1ffaa4a6917c7a311bb2e2b608964220
Timur Bazhirov <timur@exabyte.io>
2024-11-20 11:29:39 -0800

    chore: update scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-PYTHON_VERSION="3.10.12"
-NODE_VERSION="18"
+# PYTHON_VERSION="3.10.12"
+# NODE_VERSION="18"
 THIS_SCRIPT_DIR_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 PACKAGE_ROOT_PATH="$(realpath "${THIS_SCRIPT_DIR_PATH}/../")"
-BUILD_DIR_PATH="${PACKAGE_ROOT_PATH}/extensions/dist"
 REQUIREMENTS_FILENAME="requirements.txt"
+TMP_DIR="tmp"
+CONTENT_DIR="content"
 
 source "${THIS_SCRIPT_DIR_PATH}"/functions.sh
 
@@ -12,8 +13,28 @@ source "${THIS_SCRIPT_DIR_PATH}"/functions.sh
 cd "${PACKAGE_ROOT_PATH}" || exit 1
 
 [[ -n ${INSTALL} ]] && python -m pip install -r ${REQUIREMENTS_FILENAME}
-[[ -n ${COPY} ]] && cp -rL content content-resolved
-[[ -n ${BUILD} ]] && jupyter lite build --contents content --output-dir dist
+
+# Update the content dir to latest commit
+if [[ -n ${UPDATE_CONTENT} ]]; then
+    mkdir -p ${TMP_DIR} && cd ${TMP_DIR} || exit 1
+    REPO_NAME="api-examples"
+    # Clone repository if it doesn't exist
+    [[ ! -e "${REPO_NAME}" ]] && git clone https://github.com/Exabyte-io/${REPO_NAME}.git
+    cd ${REPO_NAME} || exit 1
+    git checkout main || git checkout dev && git pull
+    git --no-pager log --decorate=short --pretty=oneline -n1
+    cd - || exit 1
+    # Resolve links inside the ${REPO_NAME}
+    rm -rf ${REPO_NAME}-resolved
+    cp -rL ${REPO_NAME} ${REPO_NAME}-resolved
+    # Sync with the content directory
+    cd "${PACKAGE_ROOT_PATH}" || exit 1
+    RESOLVED_CONTENT_DIR="tmp/${REPO_NAME}-resolved"
+    rm -rf ${CONTENT_DIR} && mkdir -p ${CONTENT_DIR}
+    cp -r ${RESOLVED_CONTENT_DIR}/* ${CONTENT_DIR}
+fi
+
+[[ -n ${BUILD} ]] && jupyter lite build --contents ${CONTENT_DIR} --output-dir dist
 
 # Exit with zero (for GH workflow)
 exit 0
