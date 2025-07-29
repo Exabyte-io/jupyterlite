c1f1e07b79a7ce54cc179af8664b2b7b268fb02c
VsevolodX <vsevolodbiryukov@icloud.com>
2025-07-29 11:16:36 -0700

    update: clone then checkout branch

--- scripts/build.sh
+++ scripts/build.sh
@@ -18,11 +18,11 @@ cd "${PACKAGE_ROOT_PATH}" || exit 1
 if [[ -n ${UPDATE_CONTENT} ]]; then
     mkdir -p ${TMP_DIR} && cd ${TMP_DIR} || exit 1
     REPO_NAME="api-examples"
-    BRANCH_NAME="feature/SOF-7686"
+    BRANCH_NAME="feature/SOF-7686" # "dev"
     # Clone repository if it doesn't exist
-    [[ ! -e "${REPO_NAME}" ]] && git clone https://github.com/Exabyte-io/${REPO_NAME}.git${BRANCH_NAME}
+    [[ ! -e "${REPO_NAME}" ]] && git clone https://github.com/Exabyte-io/${REPO_NAME}.git
     cd ${REPO_NAME} || exit 1
-    git checkout main || git checkout dev && git pull
+    git checkout main || git checkout ${BRANCH_NAME} && git pull
     git --no-pager log --decorate=short --pretty=oneline -n1
     cd - || exit 1
     # Resolve links inside the ${REPO_NAME}
