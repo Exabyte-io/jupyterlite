4977af573a7211ada7f75e6d6b5066ec56d960ac
Timur Bazhirov <timur@exabyte.io>
2025-07-30 00:09:34 -0700

    chore: comment

--- scripts/build_on_netlify.sh
+++ scripts/build_on_netlify.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# NOTE: a separate script is required for the "data_bridge" extension
+# to be properly linked into the JupyterLite build process on Netlify.
+# TODO: figure out how to do it in a single script.
+
 THIS_SCRIPT_DIR_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 PACKAGE_ROOT_PATH="$(realpath "${THIS_SCRIPT_DIR_PATH}/../")"
 
@@ -11,7 +15,4 @@ echo "Creating virtual environment"
 
 create_virtualenv "${PACKAGE_ROOT_PATH}/.venv-${PYTHON_VERSION}"
 
-# NOTE: a separate script is required for the "data_bridge" extension
-# to be properly linked into the JupyterLite build process on Netlify.
-# TODO: figure out how to do it in a single script.
 INSTALL=1 UPDATE_CONTENT=1 BUILD=1 bash "${THIS_SCRIPT_DIR_PATH}"/build.sh
