9f672c5d4a696c72a12c8f97855fc70cc73f7a98
VsevolodX <vsevolodbiryukov@icloud.com>
2025-07-29 21:14:26 -0700

    update: node 18

--- scripts/build_netlify.sh
+++ scripts/build_netlify.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
-nvm install 20
-nvm use 20
+nvm install 18
+nvm use 18
 echo "Node.js version: $(node --version)"
 echo "npm version: $(npm --version)"
 npm run build
--- scripts/functions.sh
+++ scripts/functions.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 ensure_python_version_installed() {
-    local PYTHON_VERSION=$1
-    if ! pyenv versions | grep -q $PYTHON_VERSION; then
-        echo "Python $PYTHON_VERSION not found. Installing..."
-        pyenv install $PYTHON_VERSION
-    fi
-    pyenv local $PYTHON_VERSION
+  # TODO: make work with pyenv with Netlify
+#    local PYTHON_VERSION=$1
+#    if ! pyenv versions | grep -q $PYTHON_VERSION; then
+#        echo "Python $PYTHON_VERSION not found. Installing..."
+#        pyenv install $PYTHON_VERSION
+#    fi
+#    pyenv local $PYTHON_VERSION
 }
 
 ensure_node_version_installed() {
