4155a25e292886a5dbe64fffd3df8a628d90a388
VsevolodX <vsevolodbiryukov@icloud.com>
2025-08-07 19:42:26 -0700

    tmp: new ax-made

--- scripts/build.sh
+++ scripts/build.sh
@@ -32,7 +32,7 @@ pip list
 if [[ -n ${UPDATE_CONTENT} ]]; then
     mkdir -p ${TMP_DIR} && cd ${TMP_DIR} || exit 1
     REPO_NAME="api-examples"
-    BRANCH_NAME= "main"
+    BRANCH_NAME= "feature/SOF-7685"
     BRANCH_NAME_FALLBACK="dev"
 
     # Clone repository if it doesn't exist
