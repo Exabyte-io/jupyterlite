c9d8771e36f22b29efcaf99659ba4fd8c417d62d
VsevolodX <vsevolodbiryukov@icloud.com>
2026-05-16 10:58:07 -0700

    update: dev mode on netlify

--- scripts/build_on_netlify.sh
+++ scripts/build_on_netlify.sh
@@ -15,4 +15,4 @@ echo "Creating virtual environment"
 
 create_virtualenv "${PACKAGE_ROOT_PATH}/.venv-${PYTHON_VERSION}"
 
-INSTALL=1 UPDATE_CONTENT=1 BUILD=1 bash "${THIS_SCRIPT_DIR_PATH}"/build.sh
+INSTALL=1 UPDATE_CONTENT=1 BUILD=1 DEV_MODE=1 bash "${THIS_SCRIPT_DIR_PATH}"/build.sh
